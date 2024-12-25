ff2079b47232694380675fe94b6b82ae11ec0747
MinhNhat214 <nhattran200411@gmail.com>
2024-12-24 16:26:47 -0800

    commit 3 - server less

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,9 +1,20 @@
+// const express = require("express");
+// const { login } = require("../controllers/LoginController");
+
+// const router = express.Router();
+
+// // Route để lấy danh sách Users kèm Role
+// router.post("/login", login);
+
+// module.exports = router;
+
 const express = require("express");
+
 const { login } = require("../controllers/LoginController");
 
 const router = express.Router();
 
 // Route để lấy danh sách Users kèm Role
-router.post("/login", login);
+router.get("/login", login);
 
 module.exports = router;
