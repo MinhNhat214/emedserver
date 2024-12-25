53649cda293a9f0733afdec2cd07ee66795f4d8b
MinhNhat214 <nhattran200411@gmail.com>
2024-12-24 16:36:54 -0800

    commit 4 -not server less

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-// const express = require('express');
-// const cors = require('cors');
-// const app = express();
-// app.use(express.json());
-
-// app.use(cors());  // Cấu hình này sẽ cho phép tất cả các nguồn truy cập vào API
-
-// const userRoute = require('./routes/userRoute');
-// const loginRoute = require('./routes/loginRoute');
-
-// // Sử dụng route
-// app.use('/api', userRoute);
-// app.use('/api', loginRoute);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-const express = require("express");
-const { createServer } = require("@vercel/node");
-const loginRoute = require("./routes/loginRoute");
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
-
-// Middleware
 app.use(express.json());
 
-// Routes
-app.use("/api", loginRoute);
+app.use(cors());  // Cấu hình này sẽ cho phép tất cả các nguồn truy cập vào API
 
-// Export serverless function
-module.exports = createServer(app);
+const userRoute = require('./routes/userRoute');
+const loginRoute = require('./routes/loginRoute');
+
+// Sử dụng route
+app.use('/api', userRoute);
+app.use('/api', loginRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
+// const express = require("express");
+// const { createServer } = require("@vercel/node");
+// const loginRoute = require("./routes/loginRoute");
+
+// const app = express();
+
+// // Middleware
+// app.use(express.json());
+
+// // Routes
+// app.use("/api", loginRoute);
+
+// // Export serverless function
+// module.exports = createServer(app);
--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,20 +1,20 @@
-// const express = require("express");
-// const { login } = require("../controllers/LoginController");
-
-// const router = express.Router();
-
-// // Route để lấy danh sách Users kèm Role
-// router.post("/login", login);
-
-// module.exports = router;
-
 const express = require("express");
-
 const { login } = require("../controllers/LoginController");
 
 const router = express.Router();
 
 // Route để lấy danh sách Users kèm Role
-router.get("/login", login);
+router.post("/login", login);
 
 module.exports = router;
+
+// const express = require("express");
+
+// const { login } = require("../controllers/LoginController");
+
+// const router = express.Router();
+
+// // Route để lấy danh sách Users kèm Role
+// router.get("/login", login);
+
+// module.exports = router;
