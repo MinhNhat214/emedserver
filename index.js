fa0ce68596cdd17fae62ff63f1e681ced7d8b7e7
MinhNhat214 <nhattran200411@gmail.com>
2024-12-31 09:42:11 -0800

    npm i env

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 
-app.use(cors()); // Cấu hình này sẽ cho phép tất cả các nguồn truy cập vào API
+app.use(cors());  // Cấu hình này sẽ cho phép tất cả các nguồn truy cập vào API
 
-const userRoute = require("./routes/userRoute");
-const loginRoute = require("./routes/loginRoute");
+const userRoute = require('./routes/userRoute');
+const loginRoute = require('./routes/loginRoute');
 
 // Sử dụng route
-app.use("/api", userRoute);
-app.use("/api", loginRoute);
+app.use('/api', userRoute);
+app.use('/api', loginRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
